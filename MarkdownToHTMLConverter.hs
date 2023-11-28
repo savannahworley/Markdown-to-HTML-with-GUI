9ddf872bfb28ffd451463ccc24620c75a724b98f
savannahworley <worleysf@appstate.edu>
2023-11-27 22:04:29 -0500

    created Types, Tokens, and Elements of Markdown

--- MarkdownToHTMLConverter.hs
+++ MarkdownToHTMLConverter.hs
@@ -1,1 +1,16 @@
 
+type Text = String 
+
+data Token = Hash | Dash | Equal | Space | Ast 
+    | UndScore | Lt | Num | Tab | Backslash 
+    | LBra | RBra | Exclamation | BackTick 
+    | LPar | RPar | Quote | Gt | PMD Elements 
+
+data Elements = H1 Text | H2 Text | H3 Text 
+    | H4 Text | H5 Text | H6 Text | Para Text
+    | LnBreak | Bold Text | Italic Text | BoldAndItalic Text 
+    | Blockquote Text | OrderedList [Elements] | ListItem 
+    | UnorderedList [Elements] | Code Text | Image Text 
+    | HorizontalRule | Link Text
+
+    

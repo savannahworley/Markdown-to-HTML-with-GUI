071dbd611edf8098a6700f4c4f2952936f87ff57
aidancook89 <cookar4@appstate.edu>
2023-12-08 19:18:08 -0500

    Added comments to sr before starting on fixing paragraph parsing

--- src/MarkdownToHTMLConverter.hs
+++ src/MarkdownToHTMLConverter.hs
@@ -87,69 +87,81 @@ parser s = case result of
         result = sr [] s
 
 sr :: [Token] -> [Token] -> [Token]
-sr (ContentText t : Hash : ts) q = sr (PMD (H1 t) : ts) q
-sr (ContentText t : Hash2 : ts) q = sr (PMD (H2 t): ts) q
-sr (ContentText t : Hash3 : ts) q = sr (PMD (H3 t) : ts) q
-sr (ContentText t : Hash4 : ts) q = sr (PMD (H4 t) : ts) q
-sr (ContentText t : Hash5 : ts) q = sr (PMD (H5 t) : ts) q
-sr (ContentText t : Hash6 : ts) q = sr (PMD (H6 t) : ts) q
-sr (ContentText t : NewLine : NewLine : ts) q = sr (PMD (Para t) : ts) q 
-sr (SpaceChar : SpaceChar : ts) q = sr (PMD (LnBreak) : ts) q 
-sr (Ast : Ast : ContentText t : Ast : Ast : ts) q = sr (PMD (Bold t) : ts) q 
-sr (UndScore : UndScore : ContentText t : UndScore : UndScore : ts) q = sr (PMD (Bold t) : ts) q 
-sr (Ast : ContentText t : Ast : ts) q = sr (PMD (Italic t) : ts) q 
-sr (UndScore : ContentText t : UndScore : ts) q = sr (PMD (Italic t) : ts) q 
-sr (Ast : Ast : Ast : ContentText t : Ast : Ast : Ast : ts) q = sr (PMD (BoldAndItalic t) : ts) q 
-sr (UndScore : UndScore : UndScore : ContentText t : UndScore : UndScore : UndScore : ts) q = sr (PMD (BoldAndItalic t) : ts) q 
-sr (UndScore : UndScore : Ast : ContentText t : Ast : UndScore : UndScore : ts) q = sr (PMD (BoldAndItalic t) : ts) q 
-sr (Ast : Ast : UndScore : ContentText t : UndScore : Ast : Ast : ts) q = sr (PMD (BoldAndItalic t) : ts) q 
-sr (ContentText t : Gt : ts) q = sr (PMD (Blockquote t) : ts) q
 
 --ordered list code
+--nesting ordered list in unordered list
 sr (ContentText t : NumLiteral _ : Tab x : NewLine : PMD (UnorderedList x1 xs) : ts) q = sr (PMD (UnorderedList x1 (xs ++ [OrderedList x [ListItem t]])) : ts) q
+--case for orderedlists nested inside orderedlists
 sr (ContentText t : NumLiteral _ : Tab x1 : NewLine : PMD (OrderedList x xs) : ts) q | x == x1 = sr (PMD (OrderedList x (xs ++ [ListItem t])) : ts) q
     | otherwise = sr (PMD (OrderedList x (indentListHelper (OrderedList x1 [ListItem t]) xs)) : ts) q
+--combining orderedlists that are parsed, same indentation, and occur together
 sr (PMD (OrderedList i s2) : NewLine : PMD(OrderedList i1 xs) : ts) q | i == i1 = sr (PMD (OrderedList i1 (xs ++ s2)): ts) q
     | otherwise = sr (PMD (OrderedList i1 (xs ++ indentListHelper (OrderedList i s2) xs)) : ts) q
+--adding a listitem to an orderedlist
 sr (ContentText t : NewLine : PMD(OrderedList i1 xs) : ts) q = sr (PMD (OrderedList i1 (xs ++ [ListItem t])): ts) q
+--creating an orderedlist
 sr (ContentText t : NumLiteral x : ts) q = sr (PMD (OrderedList 0 [ListItem t]) : ts) q
+--case for if a nested ordered list is parsed before its identifiers are consumed
 sr (PMD (OrderedList i s2) : Tab x1 : NewLine : NumLiteral _ : NewLine : PMD (OrderedList i1 s1) : ts) q = sr (PMD (OrderedList i1 (s1 ++ [OrderedList x1 s2])) : ts) q
 
--- sr (PMD (OrderedList i s2) : NewLine : PMD(OrderedList i1 xs) : ts) q = sr (PMD (OrderedList (xs ++ s2)): ts) q
--- sr (PMD (OrderedList i s2) : Tab x1 : NewLine : NumLiteral x : NewLine : PMD (OrderedList i1 s1) : ts) q = sr (PMD (OrderedList (s1 ++ [OrderedList s2])) : ts) q
--- sr (PMD (OrderedList i s2) : Tab x1 : NewLine : PMD (OrderedList i1 s1) : ts) q = sr (PMD (OrderedList (indentListHelper s1 s2)) : ts) q
--- sr (PMD (OrderedList i s2) : Tab x1 : NewLine : NumLiteral x : ts) q = sr (PMD (OrderedList [OrderedList s2]) : ts) q
--- sr (PMD (OrderedList i s2) : Tab x1 : NewLine : ContentText s : NumLiteral x : ts) q = sr (PMD (OrderedList (ListItem s : [OrderedList s2])) : ts) q
--- sr (ContentText t : NewLine : PMD(OrderedList i1 xs) : ts) q = sr (PMD (OrderedList (xs ++ [ListItem t])): ts) q
--- sr (ContentText t : NumLiteral x : ts) q = sr (PMD (OrderedList [ListItem t]) : ts) q
-
-
 --unordered list code
+--nesting unordered list in unordered list with the different constructors for an unorderedlist
 sr (ContentText t : Dash : Tab x : NewLine : PMD (OrderedList x1 xs) : ts) q = sr (PMD (OrderedList x1 (xs ++ [UnorderedList x [ListItem t]])) : ts) q
 sr (ContentText t : Ast : Tab x : NewLine : PMD (OrderedList x1 xs) : ts) q = sr (PMD (OrderedList x1 (xs ++ [UnorderedList x [ListItem t]])) : ts) q
 sr (ContentText t : Plus : Tab x : NewLine : PMD (OrderedList x1 xs) : ts) q = sr (PMD (OrderedList x1 (xs ++ [UnorderedList x [ListItem t]])) : ts) q
+--cases for creating or adding to a nested unorderedlist
 sr (ContentText t : Dash : Tab x1 : NewLine : PMD (UnorderedList x xs) : ts) q | x == x1 = sr (PMD (UnorderedList x (xs ++ [ListItem t])) : ts) q
     | otherwise = sr (PMD (UnorderedList x (indentListHelper2 (UnorderedList x1 [ListItem t]) xs)) : ts) q
 sr (ContentText t : Ast : Tab x1 : NewLine : PMD (UnorderedList x xs) : ts) q | x == x1 = sr (PMD (UnorderedList x (xs ++ [ListItem t])) : ts) q
     | otherwise = sr (PMD (UnorderedList x (indentListHelper2 (UnorderedList x1 [ListItem t]) xs)) : ts) q
 sr (ContentText t : Plus : Tab x1 : NewLine : PMD (UnorderedList x xs) : ts) q | x == x1 = sr (PMD (UnorderedList x (xs ++ [ListItem t])) : ts) q
     | otherwise = sr (PMD (UnorderedList x (indentListHelper2 (UnorderedList x1 [ListItem t]) xs)) : ts) q
+--combining unorderedlists that are parsed, same indentation, and occur together
 sr (PMD (UnorderedList i s2) : NewLine : PMD(UnorderedList i1 xs) : ts) q | i == i1 = sr (PMD (UnorderedList i1 (xs ++ s2)): ts) q
     | otherwise = sr (PMD (UnorderedList i1 (xs ++ indentListHelper (UnorderedList i s2) xs)) : ts) q
+--adding a listitem to an unorderedlist
 sr (ContentText t : NewLine : PMD(UnorderedList i1 xs) : ts) q = sr (PMD (UnorderedList i1 (xs ++ [ListItem t])): ts) q
+
+--creating an unorderedlist
 sr (ContentText t : Dash : ts) q = sr (PMD (UnorderedList 0 [ListItem t]) : ts) q
 sr (ContentText t : Ast : ts) q = sr (PMD (UnorderedList 0 [ListItem t]) : ts) q
 sr (ContentText t : Plus : ts) q = sr (PMD (UnorderedList 0 [ListItem t]) : ts) q
+--cases for if a nested unorderedlist is parsed before its identifiers are consumed
 sr (PMD (UnorderedList i s2) : Tab x1 : NewLine : Dash : NewLine : PMD (UnorderedList i1 s1) : ts) q = sr (PMD (UnorderedList i1 (s1 ++ [UnorderedList x1 s2])) : ts) q
 sr (PMD (UnorderedList i s2) : Tab x1 : NewLine : Ast : NewLine : PMD (UnorderedList i1 s1) : ts) q = sr (PMD (UnorderedList i1 (s1 ++ [UnorderedList x1 s2])) : ts) q
 sr (PMD (UnorderedList i s2) : Tab x1 : NewLine : Plus : NewLine : PMD (UnorderedList i1 s1) : ts) q = sr (PMD (UnorderedList i1 (s1 ++ [UnorderedList x1 s2])) : ts) q
 
-
-
+--creating headers
+sr (ContentText t : Hash : ts) q = sr (PMD (H1 t) : ts) q
+sr (ContentText t : Hash2 : ts) q = sr (PMD (H2 t): ts) q
+sr (ContentText t : Hash3 : ts) q = sr (PMD (H3 t) : ts) q
+sr (ContentText t : Hash4 : ts) q = sr (PMD (H4 t) : ts) q
+sr (ContentText t : Hash5 : ts) q = sr (PMD (H5 t) : ts) q
+sr (ContentText t : Hash6 : ts) q = sr (PMD (H6 t) : ts) q
+--creating a paragraph
+sr (ContentText t : NewLine : ts) q = sr (PMD (Para t) : ts) q
+--creating a line break 
+sr (SpaceChar : SpaceChar : ts) q = sr (PMD (LnBreak) : ts) q 
+--creating bold text
+sr (Ast : Ast : ContentText t : Ast : Ast : ts) q = sr (PMD (Bold t) : ts) q 
+sr (UndScore : UndScore : ContentText t : UndScore : UndScore : ts) q = sr (PMD (Bold t) : ts) q 
+--creating italic text
+sr (Ast : ContentText t : Ast : ts) q = sr (PMD (Italic t) : ts) q 
+sr (UndScore : ContentText t : UndScore : ts) q = sr (PMD (Italic t) : ts) q 
+--creating bold and italic combo
+sr (Ast : Ast : Ast : ContentText t : Ast : Ast : Ast : ts) q = sr (PMD (BoldAndItalic t) : ts) q 
+sr (UndScore : UndScore : UndScore : ContentText t : UndScore : UndScore : UndScore : ts) q = sr (PMD (BoldAndItalic t) : ts) q 
+sr (UndScore : UndScore : Ast : ContentText t : Ast : UndScore : UndScore : ts) q = sr (PMD (BoldAndItalic t) : ts) q 
+sr (Ast : Ast : UndScore : ContentText t : UndScore : Ast : Ast : ts) q = sr (PMD (BoldAndItalic t) : ts) q
+--creating a blockquote 
+sr (ContentText t : Gt : ts) q = sr (PMD (Blockquote t) : ts) q
+--creating a code block
 sr (ContentText t : SpaceChar : SpaceChar : SpaceChar : SpaceChar : ts) q = sr (PMD (Code t) : ts) q
 sr (ContentText t : Tab x : ts) q = sr (PMD (Code t) : ts) q
 sr (BackTick : ContentText t : BackTick : ts) q = sr (PMD (Code t) : ts) q
+--creating an image
 sr (RPar : ContentText t : LPar : RBra : ContentText tx : LBra : Exclamation : ts) q = sr (PMD (Image tx t) : ts) q
+--creating horizontal rules
 sr (Ast : Ast : Ast : ts) q = sr (PMD (HorizontalRule) : ts) q 
 sr (UndScore : UndScore : UndScore : ts) q = sr (PMD (HorizontalRule) : ts) q 
 sr (Dash : Dash : Dash : ts) q = sr (PMD (HorizontalRule) : ts) q 
@@ -158,37 +170,27 @@ sr (Err e : ts) q = [Err e]
 sr ts (x:q) = sr (x:ts) q
 sr ts [] = ts
 
---needs Links, Images, HorizontalRule, OrderedList and UnOrderedList
-
+--assists in checking if the parsed orderedlist is part of a new nested list or the next part of a previous one
 indentListHelper :: Elements -> [Elements] -> [Elements]
 indentListHelper (OrderedList x xs) ((OrderedList y ys) : zs) = if x == y then [OrderedList y (ys ++ xs)] ++ zs else (OrderedList y ys) : zs ++ [OrderedList x xs]
 indentListHelper (OrderedList x xs) ys = ys ++ [OrderedList x xs]
--- indentListHelper x [] = [x]
--- indentListHelper (OrderedList x xs) (y:ys) = 
---     case y of 
---         OrderedList x1 zs -> if x == x1 then [OrderedList x1 (zs ++ xs)] ++ ys else y : indentListHelper (OrderedList x xs) ys
---         _ -> y : indentListHelper (OrderedList x xs) ys
 
+--assists in checking if the parsed unorderedlist is part of a new nested list or the next part of a previous one
 indentListHelper2 :: Elements -> [Elements] -> [Elements]
 indentListHelper2 (UnorderedList x xs) ((UnorderedList y ys) : zs) = if x == y then [UnorderedList y (ys ++ xs)] ++ zs else (UnorderedList y ys) : zs ++ [UnorderedList x xs]
 indentListHelper2 (UnorderedList x xs) ys = ys ++ [UnorderedList x xs]
--- indentListHelper2 x [] = [x]
--- indentListHelper2 (UnorderedList x xs) (y:ys) = 
---     case y of 
---         UnorderedList x1 zs -> if x == x1 then [UnorderedList x (zs ++ xs)] else y : indentListHelper2 (UnorderedList x xs) ys
---         _ -> y : indentListHelper2 (UnorderedList x xs) ys
 
 
 converter :: String -> [Token] -> String 
 converter "" [] = []
-converter s (PMD (H1 t) : xs) = s ++ "<h1>" ++ t ++ "</h1>" ++ converter s xs
-converter s (PMD (H2 t) : xs) = s ++ "<h2>" ++ t ++ "</h2>" ++ converter s xs
-converter s (PMD (H3 t) : xs) = s ++ "<h3>" ++ t ++ "</h3>" ++ converter s xs
-converter s (PMD (H4 t) : xs) = s ++ "<h4>" ++ t ++ "</h4>" ++ converter s xs
-converter s (PMD (H5 t) : xs) = s ++ "<h5>" ++ t ++ "</h5>" ++ converter s xs
-converter s (PMD (H6 t) : xs) = s ++ "<h6>" ++ t ++ "</h6>" ++ converter s xs
-converter s (PMD (Para t) : xs) = s ++ "<p>" ++ t ++ "</p>" ++ converter s xs 
-converter s (PMD (LnBreak) : xs) = s ++ "<br>" ++ converter s xs 
+converter s (PMD (H1 t) : xs) = s ++ "<h1>" ++ t ++ "</h1>\n" ++ converter s xs
+converter s (PMD (H2 t) : xs) = s ++ "<h2>" ++ t ++ "</h2>\n" ++ converter s xs
+converter s (PMD (H3 t) : xs) = s ++ "<h3>" ++ t ++ "</h3>\n" ++ converter s xs
+converter s (PMD (H4 t) : xs) = s ++ "<h4>" ++ t ++ "</h4>\n" ++ converter s xs
+converter s (PMD (H5 t) : xs) = s ++ "<h5>" ++ t ++ "</h5>\n" ++ converter s xs
+converter s (PMD (H6 t) : xs) = s ++ "<h6>" ++ t ++ "</h6>\n" ++ converter s xs
+converter s (PMD (Para t) : xs) = s ++ "<p>" ++ t ++ "</p>\n" ++ converter s xs 
+converter s (PMD (LnBreak) : xs) = s ++ "<br>\n" ++ converter s xs 
 converter s (PMD (Bold t) : xs) = s ++ "<strong>" ++ t ++ "</strong>" ++ converter s xs 
 converter s (PMD (Italic t) : xs) = s ++ "<em>" ++ t ++ "</em>" ++ converter s xs 
 converter s (PMD (BoldAndItalic t) : xs) = s ++ "<em><strong>" ++ t ++ "</strong></em>" ++ converter s xs 
